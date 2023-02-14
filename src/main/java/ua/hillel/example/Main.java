dc23b369b6239230a065f738f491bffbc4a0e19f
ownerofglory <contact@ownerofglory.com>
2023-02-14 18:47:17 +0100

    Remove private mvn repo

--- src/main/java/ua/hillel/example/Main.java
+++ src/main/java/ua/hillel/example/Main.java
@@ -1,10 +1,7 @@
 package ua.hillel.example;
 
-import com.ownerofglory.hillellib.LibClass;
-
 public class Main {
     public static void main(String[] args) {
-        LibClass.method();
         System.out.println("Hello world!");
     }
 }
